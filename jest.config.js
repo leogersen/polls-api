a168aa5950aef1e1b13dcf0e882651edb0f2257f
Leo Gersen <leogersen@outlook.com>
2022-10-19 03:39:27 -0300

    refactor: changing coverage method jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: '@shelf/jest-mongodb',
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
